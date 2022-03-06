2d766a633400b23adda1199cdfda6145da2d5e32
ohabdev <ohabdev@gmail.com>
2022-03-06 22:43:58 +0600

    user list user add and delete done

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,7 +1,85 @@
-function getUsers(req, res, next) {
-  res.render("users");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const User = require("../models/People");
+const { unlink } = require("fs");
+
+async function getUsers(req, res, next) {
+  try {
+    const users = await User.find();
+    res.render("users", {
+      users: users,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function addUser(req, res, next) {
+  let newUser;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  if (req.files && req.files.length > 0) {
+    newUser = new User({
+      ...req.body,
+      avatar: req.files[0].filename,
+      password: hashedPassword,
+    });
+  } else {
+    newUser = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
+  }
+
+  // save user or send error
+  try {
+    await newUser.save();
+    res.status(200).json({
+      message: "User was added successfully!",
+    });
+  } catch (err) {
+    console.log(err);
+    // res.status(500).json({
+    //   errors: {
+    //     common: {
+    //       msg: "Unknown error occured!",
+    //     },
+    //   },
+    // });
+  }
+}
+
+async function removeUser(req, res, next) {
+  try {
+    console.log(req.params);
+    const user = await User.findByIdAndDelete({
+      _id: req.params.id,
+    });
+    console.log(user);
+    if (user.avatar) {
+      unlink(
+        path.join(__dirname, `/../public/uploads/avatars/${user.avatar}`),
+        (err) => {
+          if (err) console.log(err);
+        }
+      );
+    }
+    res.status(200).json({
+      message: "User was deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      errors: {
+        common: {
+          msg: "Could not delete the user!",
+        },
+      },
+    });
+  }
 }
 
 module.exports = {
   getUsers,
+  addUser,
+  removeUser,
 };
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,9 +1,27 @@
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidators");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  getUsers,
+  addUser,
+  removeUser,
+} = require("../controller/usersController");
 const express = require("express");
 const router = express.Router();
-const { getUsers } = require("../controller/usersController");
-
-const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
 
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
+
+router.delete("/:id", removeUser);
 
 module.exports = router;
